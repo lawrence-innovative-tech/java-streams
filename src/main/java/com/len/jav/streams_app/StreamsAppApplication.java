c99b5cdef6480a2bf07a771a83894f1b06172946
lawrence-innoative-tech <gusantonylaragmail.com>
2024-10-20 23:02:21 +0530

    java streams initial commit

---
+++ src/main/java/com/len/jav/streams_app/StreamsAppApplication.java
@@ -0,0 +1,13 @@
+package com.len.jav.streams_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsAppApplication.class, args);
+	}
+
+}
